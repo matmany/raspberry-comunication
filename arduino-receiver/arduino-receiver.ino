c86ccf47b3a69ec96f8bc2c00c9f63feb095ddcb
mamani <matmanymat@gmail.com>
2019-08-21 19:22:22 -0300

    arduino teste

---
+++ arduino-receiver/arduino-receiver.ino
@@ -0,0 +1,30 @@
+#include "RF24.h"
+#include "printf.h"
+
+RF24 radio(7, 8);
+const byte data_pipe[6] = "00001";
+
+void setup() {
+  Serial.begin(9600);
+  printf_begin();
+  
+  radio.begin();
+  radio.setRetries(15, 15);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.openReadingPipe(1, data_pipe);
+  radio.startListening();
+}
+
+void loop() {
+  if (radio.available())
+  {
+    int payload_size = radio.getDynamicPayloadSize();
+    if (payload_size > 1)
+    {
+      char* payload = new char[payload_size + 1];
+      radio.read(payload, payload_size);
+      payload[payload_size] = '\0';
+      printf("Got Message: %s\r\n", payload);
+    }
+  }
+}
