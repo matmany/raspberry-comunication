684b8d5e077653b47c684b9b7f036c9e2375f978
mat <matmanymat@gmail.com>
2019-08-26 12:22:00 -0300

    testes

---
+++ test-send-ino/test-send-ino.ino
@@ -0,0 +1,22 @@
+#include<SPI.h>
+#include<RF24.h>
+
+// ce, csn pins
+RF24 radio(7, 8) ;
+
+void setup(void){
+  radio.begin() ;
+  radio.setPALevel(RF24_PA_MAX) ;
+  radio.setChannel(0x76) ;
+  radio.openWritingPipe(0xF0F0F0F0E1LL) ;
+  radio.enableDynamicPayloads() ;
+  radio.powerUp() ;
+  
+}
+
+void loop(void){
+  const char text[] = "Hello World!" ;
+  radio.write(&text, sizeof(text)) ;
+  delay(1000) ;
+  
+}
---
+++ teste-receive-ino/teste-receive-ino.ino
@@ -0,0 +1,48 @@
+
+#include<SPI.h>
+#include<RF24.h>
+
+// ce, csn pins
+RF24 radio(7, 8) ;
+
+void setup(void){
+  while (!Serial) ;
+  Serial.begin(9600) ;
+  
+  radio.begin() ;
+  radio.setPALevel(RF24_PA_MAX) ;
+  radio.setChannel(0x76) ;
+  radio.openWritingPipe(0xF0F0F0F0E1LL) ;
+  const uint64_t pipe = 0xE8E8F0F0E1LL ;
+  radio.openReadingPipe(1, pipe) ;
+  
+  radio.enableDynamicPayloads() ;
+  radio.powerUp() ;
+  
+}
+
+void loop(void){
+  radio.startListening() ;
+  Serial.println("Starting loop. Radio on.") ;
+  char receivedMessage[32] = {0} ;
+  if (radio.available()){
+    radio.read(receivedMessage, sizeof(receivedMessage));
+    Serial.println(receivedMessage) ;
+    Serial.println("Turning off the radio.") ;
+    radio.stopListening() ;
+    
+    String stringMessage(receivedMessage) ;
+    
+    if (stringMessage == "GETSTRING"){
+      Serial.println("Looks like they want a string!") ;
+      const char text[] = "Hello World!" ;
+      radio.write(text, sizeof(text)) ;
+      Serial.println("We sent our message.") ;
+    }
+    
+  }
+  
+  delay(100) ;
+  
+  
+}
