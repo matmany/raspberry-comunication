de55ff7659be73cefb87fb21e5bab68b277466fb
mamani <matmanymat@gmail.com>
2019-08-27 20:06:51 -0300

    deu certo, porem algum mau contato

--- arduino-transmitter/arduino-transmitter.ino
+++ arduino-transmitter/arduino-transmitter.ino
@@ -1,22 +1,28 @@
-#include<SPI.h>
-#include<RF24.h>
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
 
 // ce, csn pins
-RF24 radio(9, 10) ;
+RF24 radio(7, 8);
 
-void setup(void){
-  radio.begin() ;
-  radio.setPALevel(RF24_PA_MAX) ;
-  radio.setChannel(0x76) ;
-  radio.openWritingPipe(0xF0F0F0F0E1LL) ;
-  radio.enableDynamicPayloads() ;
-  radio.powerUp() ;
-  
+void setup(void)
+{
+  Serial.begin(115200);
+  printf_begin();
+  radio.begin();
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.setChannel(0x76);
+  radio.openWritingPipe(0xF0F0F0F0E1LL);
+  radio.enableDynamicPayloads();
+  radio.powerUp();
+  //radio.setCRCLength(0);
+  radio.printDetails();
 }
 
-void loop(void){
-  const char text[] = "Hello World!" ;
-  radio.write(&text, sizeof(text)) ;
-  delay(1000) ;
-  
+void loop(void)
+{
+  const char text[] = "Hello World!";
+  radio.write(&text, sizeof(text));
+  //delay(1000);
 }
