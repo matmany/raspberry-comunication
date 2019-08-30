b0378442654ed53f6b09e79d394edd7546fafb95
mat <matmanymat@gmail.com>
2019-08-30 18:14:27 -0300

    aparentemente havia um erro na Libpy do nrf24, o qual deixa o ultimo pino usado ce em high, então função begin de nrf24 foi adcionado metodo para alterar estado do gpio para falso

--- py-recv/recvFromArduino.py
+++ py-recv/recvFromArduino.py
@@ -2,17 +2,19 @@ import RPi.GPIO as GPIO
 from lib_nrf24 import NRF24
 import time
 import spidev
-
+#ULtimo que deu certo!!!!
 GPIO.setmode(GPIO.BCM)
-
+pin = 19
+#GPIO.setup(pin, GPIO.OUT) -- corrigido na lib!!!
+#GPIO.output(pin, False)
 #pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
 pipes = ["1Node","2Node","3Node"]
 pipe1 = b"1Node"
-pipe2 = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
+pipes2 = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
 
 radio = NRF24(GPIO, spidev.SpiDev())
-radio.begin(0, 17)
-
+radio.begin(0,pin)
+#0, 17
 radio.setPayloadSize(32)
 radio.setChannel(0x76)
 radio.setDataRate(NRF24.BR_1MBPS)
@@ -21,26 +23,38 @@ radio.setPALevel(NRF24.PA_MIN)
 radio.setAutoAck(True)
 radio.enableDynamicPayloads()
 radio.enableAckPayload()
+radio.setCRCLength(2)
 
 radio.openReadingPipe(1, pipes2[1])
 radio.printDetails()
 radio.startListening()
+#radio.print_status()
+radio.powerUp()
+try:
+ while True:
+     #radio.printDetails()
+     while not radio.available(0):
+         #print(radio.whatHappened())
+         #radio.printDetails()
+         time.sleep(1/100)
+         #print("not online")
 
-while True:
+     receivedMessage = []
+     radio.read(receivedMessage, radio.getDynamicPayloadSize())
+     print("Received: {}".format(receivedMessage))
 
-    while not radio.available(0):
-        time.sleep(1/100)
+     print("Translating our received Message into unicode characters...")
+     string = ""
 
-    receivedMessage = []
-    radio.read(receivedMessage, radio.getDynamicPayloadSize())
-    print("Received: {}".format(receivedMessage))
-
-    print("Translating our received Message into unicode characters...")
-    string = ""
-
-    for n in receivedMessage:
-        if (n >= 32 and n <= 126):
-            string += chr(n)
-    print("Our received message decodes to: {}".format(string))
-
+     for n in receivedMessage:
+         if (n >= 32 and n <= 126):
+             string += chr(n)
+     print("Our received message decodes to: {}".format(string))
+     time.sleep(1)
+except KeyboardInterrupt:
+     print("\n")
+except:
+     print("erros??")
+finally:
+     GPIO.cleanup()
 #>
