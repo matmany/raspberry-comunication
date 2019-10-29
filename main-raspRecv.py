67e5e205e6f9226d9336cfceb5def3ca3ec3b356
Mateus Mamani <mateus_mamani@hotmail.com>
2019-10-29 20:05:48 -0300

    testado main, só manda a primeira msg

--- main-raspRecv.py
+++ main-raspRecv.py
@@ -6,26 +6,41 @@ import spidev
 import socket
 
 def sendToServe(tcp, dest, msg, ):
-    tcp.connect(dest)
+    print('try connection')
+    #tcp.connect(dest)
+    print('connected')
     msg = msg.encode()
+    print('encoded')
     print('Enviando:', msg)
     tcp.sendall(msg)
+    print('sent')
     #data = tcp.recv(1024)
     #print ('Received', repr(data))
     tcp.close()
+    print('closed')
+    tcp.open()
 
-HOST = '192.168.0.23'
+HOST = '192.168.0.100'
 PORT = 9090
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
+tcp.connect(dest)
 
 GPIO.setmode(GPIO.BCM)
 pin = 19
+message = list("1")
+while len(message) <32:
+   message.append(0)
 #GPIO.setup(pin, GPIO.OUT)
 #GPIO.output(pin, False)
 #pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
 pipes = ["1Node", "2Node", "3Node"]
-pipe1 = b"1Node"
+pipe1 = "1Node"
+hexaCode = []
+for letter in pipe1:
+     hexaCode.append(ord(letter))
+test = list(reversed(hexaCode))
+#hexaCode.append(0)
 pipes2 = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
 
 radio = NRF24(GPIO, spidev.SpiDev())
@@ -41,11 +56,14 @@ radio.enableDynamicPayloads()
 radio.enableAckPayload()
 radio.setCRCLength(2)
 
-radio.openReadingPipe(1, pipes2[1])
+radio.openWritingPipe(pipes2[0])
+#radio.openReadingPipe(1, pipes2[1])
+radio.openReadingPipe(1,test)
 radio.printDetails()
 radio.startListening()
 # radio.print_status()
 radio.powerUp()
+#print("1Node".enconde("hex"))
 try:
     while True:
         # radio.printDetails()
@@ -55,23 +73,28 @@ try:
             time.sleep(1/100)
             #print("not online")
 
-        receivedMessage = []
-        radio.read(receivedMessage, radio.getDynamicPayloadSize())
-        print("Received: {}".format(receivedMessage))
+        while radio.available(0):
+            receivedMessage = []
+            radio.read(receivedMessage, radio.getDynamicPayloadSize())
+            print("Received: {}".format(receivedMessage))
 
-        print("Translating our received Message into unicode characters...")
-        string = ""
+            print("Translating our received Message into unicode characters...")
+            string = ""
 
-        for n in receivedMessage:
-            if (n >= 32 and n <= 126):
-                string += chr(n)
-        print("Our received message decodes to: {}".format(string))
-        sendToServe(tcp,dest,string)
-        time.sleep(1)
+            for n in receivedMessage:
+               if (n >= 32 and n <= 126):
+                   string += chr(n)
+            print("Our received message decodes to: {}".format(string))
+            sendToServe(tcp,dest,string)
+            time.sleep(1)
+
+        radio.stopListening()
+        radio.write(message)
+        radio.startListening()
 except KeyboardInterrupt:
     print("\n")
 except:
     print("erros??")
 finally:
     GPIO.cleanup()
-# >
+
--- socket/server/serve.py
+++ socket/server/serve.py
@@ -1,10 +1,11 @@
 import socket
-HOST = '192.168.0.23'
+HOST = '192.168.0.100'
 PORT = 9090
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
-tcp.listen(1)
+tcp.listen()
+#tcp.listen(1)
 while True:
     con, client = tcp.accept()
     print ('Conectado a', client)
@@ -15,7 +16,7 @@ while True:
             con.close() 
             break
         print ('Recebendo:', client, msg)
-        con.sendall(b'serveToYou998882943')
+        #con.sendall(b'serveToYou998882943')
         #print ('Finalizando conexao do cliente', client)
         #con.close()
 
