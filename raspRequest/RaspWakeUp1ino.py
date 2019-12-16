1ea1cdddcf39a8370e50123429f72da9924dc011
mat <matmanymat@gmail.com>
2019-12-16 13:12:53 -0200

    Codigo atualizado, arduino dormindo de boa e acordando na h certa, testado só com 1

--- raspRequest/RaspWakeUp1ino.py
+++ raspRequest/RaspWakeUp1ino.py
@@ -6,65 +6,53 @@ import spidev
 GPIO.setmode(GPIO.BCM)
 pin = 19
 message = list("1")
+
 while len(message) <32:
    message.append(0)
-#GPIO.setup(pin, GPIO.OUT)
-#GPIO.output(pin, False)
-#pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
+
 pipes = ["1Node", "2Node", "3Node"]
 pipe1 = "1Node"
 hexaCode = []
+
 for letter in pipe1:
      hexaCode.append(ord(letter))
+
 test = list(reversed(hexaCode))
-#hexaCode.append(0)
 pipes2 = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
 channels = [0x76,0x78]
 radio = NRF24(GPIO, spidev.SpiDev())
-
-def arduinoWakeUp():
-    contador = 0
-    message = list("W")
-    while len(message) < 5:
-        message.append(0)
-    while (radio.write(message) or (contador < 100)):
-      contador = contador + 1
-    else:
-        print(contador)
-
-    print("Mensagems enviada: {}".format(message))
-    radio.startListening()
-
 radio.begin(0, pin)
-#0, 17
 radio.setPayloadSize(32)
 radio.setChannel(channels[0])
 radio.setDataRate(NRF24.BR_1MBPS)
 radio.setPALevel(NRF24.PA_MIN)
-
 radio.setAutoAck(True)
 radio.enableDynamicPayloads()
 radio.enableAckPayload()
 radio.setCRCLength(2)
-
 radio.openWritingPipe(pipes2[0])
-#radio.openReadingPipe(1, pipes2[1])
-radio.openReadingPipe(1,pipes2[1]) #test
+radio.openReadingPipe(1,pipes2[1])
 radio.printDetails()
-#radio.startListening()
-# radio.print_status()
 radio.powerUp()
-#print("1Node".enconde("hex"))
 channel = 0
+
+message = list("GETSTRING")
+
+while len(message) < 32:
+ message.append(0)
 try:
     while True:
+        #Acorda Arduino
         start = time.time()
-        arduinoWakeUp()
-
+        radio.write(message)
+        print("Sent the message: {}".format(message))
+        radio.startListening()
+        
         while not radio.available(0):
-            time.sleep(1/100) 
+            time.sleep(1/100)
 
         while radio.available(0):
+        #Recebe Msg de Arduino
             receivedMessage = []
             radio.read(receivedMessage, radio.getDynamicPayloadSize())
             print("Received: {}".format(receivedMessage))
@@ -76,17 +64,8 @@ try:
                if (n >= 32 and n <= 126):
                    string += chr(n)
             print("Our received message decodes to: {}".format(string))
-            time.sleep(2)
-
-        # radio.stopListening()
-        # if channel == 1:
-        #  channel = 0
-        # else:
-        #  channel+=1
-        # radio.setChannel(channels[channel])
-        # radio.write(message)
-        #print("------Channel:", channel)
-        #radio.startListening()
+            radio.stopListening()
+            time.sleep(1)
 except KeyboardInterrupt:
     print("\n")
 except:
